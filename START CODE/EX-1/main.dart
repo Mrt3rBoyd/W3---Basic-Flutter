3f15756b222f72d3fcf0bf3a7d88ee4e94a73729
Mrt3rBoyd <lsophy065@gmail.com>
2026-05-23 22:10:41 +0700

    ex1

--- START CODE/EX-1/main.dart
+++ START CODE/EX-1/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(
-    const MaterialApp( ),
-  );
+  runApp(const MaterialApp());
 }
+
+class MyApp extends StatelessWdget {
+  const MyApp({key? key}){
+    myApp(this.runApp(super.key));
+
+  @override
+  Widget build(){
+
+  }
+
+  }
+}
+
+
+
+
+
+
+//Scaffold-class.html
+//Center-class.html
+//Text (widgets/text)
