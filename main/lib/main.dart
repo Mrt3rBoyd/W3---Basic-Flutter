2de3af4e8c07fad83ef2cf0a98219185c0b7bc83
sophy <lsophy065@gmail.com>
2026-05-25 15:31:09 +0700

    update

---
+++ main/lib/main.dart
@@ -0,0 +1,163 @@
+/*
+
+//Ex1
+import "package:flutter/material.dart";
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Text(
+          "Hello my name is Ronan!",
+          
+          
+        ),
+      ),
+    ),
+  );
+} 
+
+
+//Scaffold-class.html
+//Center-class.html
+//Text (widgets/text)
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Ex2
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Container(  
+      color:Colors.blue[600],
+      width:300,
+      height:500,
+    ), 
+    margin: EdgeInsets.all(),
+    padding: EdgeInsets(),
+      child: Container(
+        decoration: BoxDecoration(
+          child: Center( 
+            child: Text "CADT STUDENT",
+            style: TextStyle
+          ),
+        ),
+      ),      
+    ),
+  ),
+}
+
+/*
+Container class
+BoxDecoration 
+EdgeInsets 
+Text widget 
+TextStyle 
+Center 
+(Optional) Column 
+*/
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//Ex 3
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Container( 
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Container( 
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Text( 
+                'OOP', style: TextStyle()            
+              ),
+            ),
+          ),
+          Container( 
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: Center(
+              child: Text( 
+                'DART', style: TextStyle()            
+              ),
+            ),
+          ),
+          Container( 
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Text( 
+                'FLUTTER', style: TextStyle()            
+              ),
+            ),
+          ),
+        ]
+      ),
+    ),
+  ));
+}
+
+
+
+/*
+Container class
+Column class
+BoxDecoration 
+LinearGradient 
+EdgeInsets 
+Text widget
+Alignment 
+*/
+
+*/
